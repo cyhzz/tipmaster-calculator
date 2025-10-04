66eb831df3a141a05b9a3f0de44c5cfda76e32fb
cyhzz <1076357483@qq.com>
2025-10-05 04:05:45 +0800

    Update route.js

--- app/api/creem/checkout/route.js
+++ app/api/creem/checkout/route.js
@@ -1,6 +1,7 @@
 // app/api/creem/checkout/route.js
 import { NextResponse } from 'next/server';
 import { getServerSession } from "next-auth";
+import { authOptions } from "../[...nextauth]/route";
 
 export async function POST(request) {
     try {
